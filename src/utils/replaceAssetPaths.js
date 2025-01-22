c30ac7f4ba98be31c46c76082925252266d24c5e
taoguangc <taoguangc@gmail.com>
2025-01-22 09:54:29 +0800

    add prebuild cmd  to replace assets path

--- src/utils/replaceAssetPaths.js
+++ src/utils/replaceAssetPaths.js
@@ -1,15 +1,15 @@
 // all .md files in this directory will be processed
 // specify from the root of your project
-const postsDirectory = '/src/content/posts/';
+const postsDirectory = '/src/content/'
 
 // Find and replace strings
-const find = '/src/assets/';
-let replace = '../../../../../assets/'; // update this path based on where files are located
+const find = '/src/images/'
+let replace = '../../images/' // update this path based on where files are located
 
-replace = replace.replaceAll('.', '\\.');
+replace = replace.replaceAll('.', '\\.')
 // Special characters (https://en.wikipedia.org/wiki/Regular_expression#POSIX_basic_and_extended) need to be escaped
 
-import { exec } from 'child_process';
+import { exec } from 'child_process'
 // execute bash command
 exec(
   `find ${process.cwd()}${postsDirectory} -type f -name '*.md' -print0 | xargs -0 sed -i -e 's:${find}:${replace}:g'`,
@@ -17,15 +17,15 @@ exec(
   (error, stdout, stderr) => {
     // error handling
     if (error) {
-      console.log(`error: ${error.message}`);
-      return;
+      console.log(`error: ${error.message}`)
+      return
     }
     if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
+      console.log(`stderr: ${stderr}`)
+      return
     }
     // success
-    console.log(stdout);
-    console.log('🖼️ Successfully replaced asset paths');
+    console.log(stdout)
+    console.log('🖼️ Successfully replaced asset paths')
   }
-);
+)
